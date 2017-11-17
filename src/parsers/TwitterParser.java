01e98e0d5b018b12398be7fcc8cb93dd877f73d8
Guillem Garcia Sabate <ggsabate@gmail.com>
2017-11-17 13:10:34 +0100

    Update TwitterParser.java

--- src/parsers/TwitterParser.java
+++ src/parsers/TwitterParser.java
@@ -6,13 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import twitter4j.Status;
-/*import twitter4j.Query;
-import twitter4j.QueryResult;
-
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;*/
 
 public class TwitterParser {
 	
